1cf8da340a96036dccf3a3219386942b43c6398e
Jeff Sell <jtsell@gmail.com>
2024-04-25 17:17:52 -0400

    Added copilot

---
+++ lua/copilot_config.lua
@@ -0,0 +1,3 @@
+vim.g.copilot_no_tab_map = true
+vim.api.nvim_set_keymap('i', '<F14>', 'copilot#Accept("<CR>")', {silent=true, expr=true})
+vim.api.nvim_set_keymap('i', '<F15>', '<Plug>(copilot-accept-line)', {silent=true })
--- lua/nerveaux_config.lua
+++
@@ -1,8 +0,0 @@
-require 'nerveux'.setup {
-  neuron_dir = "/Users/jeff/Documents/neuron",
-  create_default_mappings = true,
-  mappings = {
-    new = "<Leader>zn"
-  },
-  print("nerveaux")
-}
