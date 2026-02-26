9e2407ad948fb281cb7530c804781de83e8ce159
Jeff Sell <jtsell@gmail.com>
2026-02-26 15:50:11 -0500

    Format plus added replace binding

--- init.lua
+++ init.lua
@@ -1,10 +1,9 @@
 vim.deprecate = function() end
 
-require 'plugins'
-require 'theme'
+require("plugins")
+require("theme")
 
 local map = vim.api.nvim_set_keymap
-
 
 --g.prettier#autoformat = 1
 --g.prettier#autoformat_config_present = 1
@@ -27,26 +26,26 @@ local map = vim.api.nvim_set_keymap
 -- o.nowritebackup=true
 
 vim.o.autoindent = true
-vim.o.clipboard = 'unnamedplus'
+vim.o.clipboard = "unnamedplus"
 vim.o.cmdheight = 1
 vim.o.copyindent = true
-vim.o.encoding = 'utf-8'
+vim.o.encoding = "utf-8"
 vim.o.expandtab = true
 vim.o.foldenable = true
 vim.o.foldlevelstart = 10
-vim.o.foldmethod = 'expr'
-vim.o.foldexpr = 'nvim_treesitter#foldexpr()'
+vim.o.foldmethod = "expr"
+vim.o.foldexpr = "nvim_treesitter#foldexpr()"
 vim.o.foldnestmax = 10
 vim.o.hidden = true
 vim.o.ignorecase = true
 vim.o.incsearch = true
-vim.o.mouse = 'a'
+vim.o.mouse = "a"
 vim.o.nu = true
 vim.o.relativenumber = true
 vim.o.shiftwidth = 2
 vim.o.showcmd = true
 vim.o.showmatch = true
-vim.o.signcolumn = 'yes'
+vim.o.signcolumn = "yes"
 vim.o.smartcase = true
 vim.o.smartindent = true
 vim.o.smarttab = true
@@ -61,45 +60,49 @@ vim.g.AutoPairsMapCR = 0
 vim.g.clj_fmt_autosave = 0
 vim.g.clojure_align_multiline_strings = 1
 vim.g.clojure_align_subforms = 1
-vim.g.indentLine_char = '│'
+vim.g.indentLine_char = "│"
 vim.g.mapleader = " "
 vim.g.netrw_keepdir = 1
-vim.g.markdown_fenced_languages = { 'clojure' }
+vim.g.markdown_fenced_languages = { "clojure" }
 
 vim.wo.wrap = false
 -- vim.wo.wrap = true
 -- vim.wo.linebreak = true
 -- vim.wo.breakindent = true
 
-
-map('n', '<SPACE>', '<Nop>', { noremap = true })
-map('i', 'jj', '<esc>', { noremap = true })
-map('i', 'jk', '<esc>', { noremap = true })
-map('i', 'kj', '<esc>', { noremap = true })
-map('i', 'kk', '<esc>', { noremap = true })
-map('n', ',', ';', { noremap = true })
-map('n', ';', ':', { noremap = true })
-map('n', '<C-a>', '^', { noremap = true })
-map('n', '<C-e>', '$', { noremap = true })
-map('n', '<F1>', '<Nop>', { noremap = true })
-map('n', '<SPACE>', '<Nop>', { noremap = true })
-map('n', '<leader><space>', ':nohlsearch<CR>', { noremap = true })
-map('n', '<leader>yF', ':let @*=expand("%:p")<CR>|', { noremap = true })
-map('n', '<leader>yf', ':let @*=expand("%")<CR>|', { noremap = true })
+map("n", "<SPACE>", "<Nop>", { noremap = true })
+map("i", "jj", "<esc>", { noremap = true })
+map("i", "jk", "<esc>", { noremap = true })
+map("i", "kj", "<esc>", { noremap = true })
+map("i", "kk", "<esc>", { noremap = true })
+map("n", ",", ";", { noremap = true })
+map("n", ";", ":", { noremap = true })
+map("n", "<C-a>", "^", { noremap = true })
+map("n", "<C-e>", "$", { noremap = true })
+map("n", "<F1>", "<Nop>", { noremap = true })
+map("n", "<SPACE>", "<Nop>", { noremap = true })
+map("n", "<leader><space>", ":nohlsearch<CR>", { noremap = true })
+map("n", "<leader>yF", ':let @*=expand("%:p")<CR>|', { noremap = true })
+map("n", "<leader>yf", ':let @*=expand("%")<CR>|', { noremap = true })
 -- map('n', 'BD', ':bd<CR>', { noremap = true })
-map('n', 'gB', ':bprevious<CR>', { noremap = true })
-map('n', 'gb', ':bnext<CR>', { noremap = true })
-map('v', ',', ';', { noremap = true })
-map('v', ';', ':', { noremap = true })
-map('n', 'th', ':tabm -1<CR>', { noremap = true })
-map('n', 'tl', ':tabm +1<CR>', { noremap = true })
-map('n', '<leader>ww', ':w<CR>', { noremap = true })
-map('n', '<leader>wq', ':wq<CR>', { noremap = true })
-map('t', '<ESC><ESC>', '<C-\\><C-n>', { noremap = true })
+map("n", "gB", ":bprevious<CR>", { noremap = true })
+map("n", "gb", ":bnext<CR>", { noremap = true })
+map("v", ",", ";", { noremap = true })
+map("v", ";", ":", { noremap = true })
+map("n", "th", ":tabm -1<CR>", { noremap = true })
+map("n", "tl", ":tabm +1<CR>", { noremap = true })
+map("n", "<leader>ww", ":w<CR>", { noremap = true })
+map("n", "<leader>wq", ":wq<CR>", { noremap = true })
+map("t", "<ESC><ESC>", "<C-\\><C-n>", { noremap = true })
+map("n", "<Leader>pp", 'V"0p', { noremap = true })
+map("v", "<Leader>pp", '"0p', { noremap = true })
 
-map('n', '<localleader>po',
-  ":ConjureEval (require 'portal.api) (portal.api/tap) (portal.api/open {:theme :portal.colors/gruvbox :app false})<CR>",
-  { noremap = true })
+map(
+	"n",
+	"<localleader>po",
+	":ConjureEval (require 'portal.api) (portal.api/tap) (portal.api/open {:theme :portal.colors/gruvbox :app false})<CR>",
+	{ noremap = true }
+)
 
 vim.cmd([[command! Less runtime pager.vim]])
 -- vim.cmd([[
@@ -111,13 +114,13 @@ vim.cmd([[command! Less runtime pager.vim]])
 --
 vim.cmd([[autocmd FileType graphql setlocal commentstring=#\ %s]])
 
-map('n', 'gl', "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
+map("n", "gl", "<cmd>lua vim.diagnostic.open_float()<CR>", { noremap = true, silent = true })
 
-vim.api.nvim_create_autocmd({"BufEnter", "BufWinEnter"}, {
-  callback = function()
-    vim.opt.formatoptions:remove({ 'r', 'o' })
-  end
+vim.api.nvim_create_autocmd({ "BufEnter", "BufWinEnter" }, {
+	callback = function()
+		vim.opt.formatoptions:remove({ "r", "o" })
+	end,
 })
-vim.opt_global.formatoptions:remove({ 'r', 'o' })
+vim.opt_global.formatoptions:remove({ "r", "o" })
 
-map('n', 'gcp', ':yank | Commentary | put<CR>', { noremap = true })
+map("n", "gcp", ":yank | Commentary | put<CR>", { noremap = true })
